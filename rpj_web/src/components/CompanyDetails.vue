4765135402ac7f0994abe2d8584ddae98cdba45c
chikwongyip <527439024@qq.com>
2023-04-17 11:08:48 +0800

    Company view

--- rpj_web/src/components/CompanyDetails.vue
+++ rpj_web/src/components/CompanyDetails.vue
@@ -1,45 +1,25 @@
 <template>
-  <el-form ref="form" :model="form" label-width="80px">
-    <el-form-item label="活动名称">
-      <el-input v-model="form.name" label-width="80px"></el-input>
+  <el-form ref="form" :model="company" class="company_form">
+    <el-form-item label="公司名称" class="item">
+      <el-input v-model="company.name"  class="name"></el-input>
     </el-form-item>
-    <el-form-item label="活动区域">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
+    <el-form-item label="公司介绍" class="item">
+      <el-input type="textarea" :rows="4" placeholder="请输入内容" :model="company.description" class="description"></el-input>
     </el-form-item>
-    <el-form-item label="活动时间">
-      <el-col :span="11">
-        <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
-      </el-col>
-      <el-col class="line" :span="2">-</el-col>
-      <el-col :span="11">
-        <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
-      </el-col>
+    <el-form-item label="ICP" class="item">
+      <el-input v-model="company.icp"  class="name"></el-input>
     </el-form-item>
-    <el-form-item label="即时配送">
-      <el-switch v-model="form.delivery"></el-switch>
+    <el-form-item label="tel" class="item">
+      <el-input v-model="company.tel"  class="name"></el-input>
     </el-form-item>
-    <el-form-item label="活动性质">
-      <el-checkbox-group v-model="form.type">
-        <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-        <el-checkbox label="地推活动" name="type"></el-checkbox>
-        <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-        <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-      </el-checkbox-group>
+    <el-form-item label="address" class="item">
+      <el-input type="textarea" :rows="4" placeholder="请输入内容" :model="company.address" class="description"></el-input>
     </el-form-item>
-    <el-form-item label="特殊资源">
-      <el-radio-group v-model="form.resource">
-        <el-radio label="线上品牌商赞助"></el-radio>
-        <el-radio label="线下场地免费"></el-radio>
-      </el-radio-group>
-    </el-form-item>
-    <el-form-item label="活动形式">
-      <el-input type="textarea" v-model="form.desc"></el-input>
+    <el-form-item label="email" class="item">
+      <el-input v-model="company.email"  class="name"></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      <el-button type="primary" @click="onSubmit">修改</el-button>
       <el-button>取消</el-button>
     </el-form-item>
   </el-form>
@@ -50,15 +30,15 @@ export default {
   name: "CompanyDetails",
   data() {
     return {
-      form: {
+      company: {
         name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        description: '',
+        icp: '',
+        tel: '',
+        address: "",
+        email: '',
+        logo: '',
+        thumb_logo: ''
       }
     }
   },
@@ -72,5 +52,17 @@ export default {
 </script>
 
 <style scoped>
-
+  .company_form{
+    label-width:80px;
+    text-align:left;
+    .item{
+      width:100px;
+      .name{
+        width: 200px;
+      }
+      .description{
+        width: 400px;
+      }
+    }
+  }
 </style>
