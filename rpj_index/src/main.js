e2a874f7285491a053734fff78413166f25f6607
chikwongyip <527439024@qq.com>
2023-06-14 08:05:04 +0800

    init project

--- rpj_index/src/main.js
+++ rpj_index/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import ElementUI from "element-ui";
+import axios from "axios"
+import "element-ui/lib/theme-chalk/index.css";
 import App from './App.vue'
-
+Vue.use(ElementUI)
+Vue.use(VueAxios,axios)
 Vue.config.productionTip = false
 
 new Vue({
